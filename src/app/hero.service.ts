58456b8aef4447b8a04a52ef1588f3efcd685160
mjfrancki <mikejfrancki@gmail.com>
2019-10-25 09:51:33 -0400

    2 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,7 +20,7 @@ export class HeroService {
     private messageService: MessageService) { }
     
 /** GET heroes from the server */
-getHeroes (): Observable<Hero[]> {
+getHeroes(): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl).pipe(
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
