5ebb6d2aa0eda99f06332586c3c792e1e32301cb
mjfrancki <mikejfrancki@gmail.com>
2019-10-31 16:18:15 -0400

    2 files modified, 1 added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  todaydate = new Date();
+
+  months = ["January", "Feburary", "March" , "April", "May" , "June", "July", "August", "September", "October", "November", "December"];
+
+  isavailable = true; 
+
+  myClickFunction(event) { 
+    alert("Button is clicked");
+    console.log(event);
+  } 
+
+  changemonths(event) {
+    alert("Changed month from the Dropdown");
+    console.log(event);
+  }
+
   constructor() { }
 
   ngOnInit() {
